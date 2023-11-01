36d4742dce5dbd8480f14b14eb71f04f8d275e1f
satvshr <112589278+satvshr@users.noreply.github.com>
2023-11-01 21:28:27 +0530

    REST API integration done

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,18 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from quiz.views import *
-from django.conf import settings
-from django.conf.urls.static import static
  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home,name='home'),
-    path('addQuestion/', addQuestion,name='addQuestion'),
-    path('login/', loginPage,name='login'),
-    path('logout/', logoutPage,name='logout'),
-    path('register/', registerPage,name='register'),
- 
+     path('', include('quiz.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
