58552daedd22fae46dd7feb1725dc61aeed19c41
satvshr <112589278+satvshr@users.noreply.github.com>
2023-11-21 11:56:53 +0530

    Made the table viewing more convenient and added tables for all the websites

--- backend/quiz/admin.py
+++ backend/quiz/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
-admin.site.register(Question)
+from .models import *
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question','op1','op2','op3','op4','ans','hint')
+admin.site.register(Question, QuestionAdmin)
+
+class PromptedAdmin(admin.ModelAdmin):
+    list_display = ('question_number','option_click_time','help_button_click_time','continue_button_click_time','time_spent_on_question')
+admin.site.register(Prompted, PromptedAdmin)
+
+class UnpromptedAdmin(admin.ModelAdmin):
+    list_display = ('question_number','option_click_time','help_button_click_time','continue_button_click_time','time_spent_on_question')
+admin.site.register(Unprompted, UnpromptedAdmin)
+
+class NoAssistanceAdmin(admin.ModelAdmin):
+    list_display = ('question_number','option_click_time','help_button_click_time','continue_button_click_time','time_spent_on_question')
+admin.site.register(NoAssistance, NoAssistanceAdmin)
--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -11,5 +11,40 @@ class Question(models.Model):
     ans = models.CharField(max_length=200,null=True)
     hint = models.TextField(null=True)
 
-    def __str__(self):
-        return self.question
+    # def __str__(self):
+    #     return self.question
+    
+class Prompted(models.Model):
+    question_number = models.AutoField(primary_key=True)
+    option_click_time = models.DurationField()
+    help_button_click_time = models.DurationField()
+    continue_button_click_time = models.DurationField()
+    time_spent_on_question = models.DurationField()
+    option_chosen = models.JSONField(default=list)
+
+    # def __str__(self):
+    #     return self.question_number
+
+class Unprompted(models.Model):
+    question_number = models.AutoField(primary_key=True)
+    option_click_time = models.DurationField()
+    help_button_click_time = models.DurationField()
+    continue_button_click_time = models.DurationField()
+    time_spent_on_question = models.DurationField()
+    option_chosen = models.JSONField(default=list)
+    
+class NoAssistance(models.Model):
+    question_number = models.AutoField(primary_key=True)
+    option_click_time = models.DurationField()
+    help_button_click_time = models.DurationField()
+    continue_button_click_time = models.DurationField()
+    time_spent_on_question = models.DurationField()
+    option_chosen = models.JSONField(default=list)
+
+# class Misleading(models.Model):
+#     question_number = models.AutoField(primary_key=True)
+#     option_click_time = models.DurationField()
+#     help_button_click_time = models.DurationField()
+#     continue_button_click_time = models.DurationField()
+#     time_spent_on_question = models.DurationField()
+#     option_chosen = models.JSONField(default=list)
