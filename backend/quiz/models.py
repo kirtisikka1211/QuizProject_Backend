3e7689956d963430bae1a3f982915b380928c54f
satvshr <112589278+satvshr@users.noreply.github.com>
2023-11-22 18:12:33 +0530

    updated table names and made custom table views as discussed

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -13,33 +13,49 @@ class Question(models.Model):
 
     # def __str__(self):
     #     return self.question
-    
-class Prompted(models.Model):
-    question_number = models.AutoField(primary_key=True)
-    option_click_time = models.DurationField()
-    help_button_click_time = models.DurationField()
-    continue_button_click_time = models.DurationField()
-    time_spent_on_question = models.DurationField()
-    option_chosen = models.JSONField(default=list)
 
-    # def __str__(self):
-    #     return self.question_number
+class User(models.Model):
+    roll_no = models.CharField(max_length=200,unique=True)
+    name = models.CharField(max_length=200)
+    set_attempted = models.IntegerField(null=False)
 
-class Unprompted(models.Model):
-    question_number = models.AutoField(primary_key=True)
+class UserAnswers(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_number = models.ForeignKey(Question, on_delete=models.CASCADE)
+    set_attempted = models.IntegerField(null=False)
     option_click_time = models.DurationField()
     help_button_click_time = models.DurationField()
     continue_button_click_time = models.DurationField()
     time_spent_on_question = models.DurationField()
-    option_chosen = models.JSONField(default=list)
+    option_chosen = models.CharField(max_length=1)
+    is_correct = models.BooleanField(null=False)
+
+# class Prompted(models.Model):
+#     question_number = models.AutoField(primary_key=True)
+#     option_click_time = models.DurationField()
+#     help_button_click_time = models.DurationField()
+#     continue_button_click_time = models.DurationField()
+#     time_spent_on_question = models.DurationField()
+#     option_chosen = models.JSONField(default=list)
+
+#     # def __str__(self):
+#     #     return self.question_number
+
+# class Unprompted(models.Model):
+#     question_number = models.AutoField(primary_key=True)
+#     option_click_time = models.DurationField()
+#     help_button_click_time = models.DurationField()
+#     continue_button_click_time = models.DurationField()
+#     time_spent_on_question = models.DurationField()
+#     option_chosen = models.JSONField(default=list)
     
-class NoAssistance(models.Model):
-    question_number = models.AutoField(primary_key=True)
-    option_click_time = models.DurationField()
-    help_button_click_time = models.DurationField()
-    continue_button_click_time = models.DurationField()
-    time_spent_on_question = models.DurationField()
-    option_chosen = models.JSONField(default=list)
+# class NoAssistance(models.Model):
+#     question_number = models.AutoField(primary_key=True)
+#     option_click_time = models.DurationField()
+#     help_button_click_time = models.DurationField()
+#     continue_button_click_time = models.DurationField()
+#     time_spent_on_question = models.DurationField()
+#     option_chosen = models.JSONField(default=list)
 
 # class Misleading(models.Model):
 #     question_number = models.AutoField(primary_key=True)
