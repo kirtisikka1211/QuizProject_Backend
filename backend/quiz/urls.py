bd1fdcd6a361816e9586fdfc4df9e979fd59c088
satvshr <112589278+satvshr@users.noreply.github.com>
2023-11-27 20:49:59 +0530

    registered the url endpoints

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuestionViewSet
+from .views import *
 
 # Create a router
 router = DefaultRouter()
 
 # Register the ItemViewSet with the router
 router.register(r'questions', QuestionViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'prompted', PromptedAnswersViewSet)
+router.register(r'unprompted', UnpromptedAnswersViewSet)
+router.register(r'noassistance', NoAssistanceAnswersViewSet)
 
 # Define your API URL patterns
 urlpatterns = [
