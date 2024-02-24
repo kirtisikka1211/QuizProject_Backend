72320c528d9173787e5471125bd437dd5060dc6d
akshaya <akshayakrishnan318@gmail.com>
2024-02-25 03:34:37 +0530

    completed feedback form with post function

--- quizapp/frontend/src/components/FeedbackForm.js
+++ quizapp/frontend/src/components/FeedbackForm.js
@@ -7,7 +7,8 @@ const FeedbackForm = () => {
   const [questions, setQuestions] = useState([]);
   const [selectedOptions, setSelectedOptions] = useState({});
   const [questionCounter, setQuestionCounter] = useState(1);
-  const options = ['A', 'B', 'C', 'D', 'E']; 
+  const [allQuestionsAnswered, setAllQuestionsAnswered] = useState(false); // State to track if all questions are answered
+  const options = ["A", "B", "C", "D", "E"];
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const roll_no = queryParams.get("roll_no");
@@ -18,11 +19,12 @@ const FeedbackForm = () => {
   const sec = ("0" + date.getSeconds()).slice(-2);
   const curtime = `${hr}:${min}:${sec}`;
 
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("https://hci-analysis.software/api/feedback/");
+        const response = await axios.get(
+          "https://hci-analysis.software/api/feedback/"
+        );
         if (
           response.data &&
           Array.isArray(response.data) &&
@@ -30,7 +32,7 @@ const FeedbackForm = () => {
         ) {
           setQuestions(response.data);
           const initialSelectedOptions = {};
-          response.data.forEach(question => {
+          response.data.forEach((question) => {
             initialSelectedOptions[question.id] = null;
           });
           setSelectedOptions(initialSelectedOptions);
@@ -44,28 +46,39 @@ const FeedbackForm = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    // Check if all questions are answered
+    const answeredQuestions = Object.values(selectedOptions).filter(
+      (option) => option !== null
+    );
+    setAllQuestionsAnswered(answeredQuestions.length === questions.length);
+  }, [selectedOptions, questions]);
+
   const handleOptionSelect = (questionId, optionIndex) => {
     const selectedOption = options[optionIndex];
-    setSelectedOptions(prevState => ({
+    setSelectedOptions((prevState) => ({
       ...prevState,
-      [questionId]: selectedOption
+      [questionId]: selectedOption,
     }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
       for (let i = 0; i < questions.length; i++) {
         const question = questions[i];
         const feedbackData = {
-          user: roll_no, 
+          user: roll_no,
           action: selectedOptions[question.id],
-          set: 1, 
+          set: 1,
           time: curtime,
-          page: `Question ${i + 1}`, 
+          page: `Question ${i + 1}`,
         };
-        await axios.post("https://hci-analysis.software/api/feedbackans/", feedbackData);
+        await axios.post(
+          "https://hci-analysis.software/api/feedbackans/",
+          feedbackData
+        );
       }
 
       navigate("/thankyou");
@@ -83,26 +96,47 @@ const FeedbackForm = () => {
         <div className="flex flex-col p-4 space-y-7 ">
           {questions.map((question, index) => (
             <div key={question.id}>
-              <p>Q.{questionCounter + index}. {question.questions}</p>
+              {/* {console.log(question)} */}
+              <p>
+                Q.{questionCounter + index}. {question.questions}
+              </p>
               <div className="space-y-2">
                 {options.map((option, optionIndex) => (
                   <div key={option} className="flex items-center">
                     <input
                       type="radio"
-                      checked={selectedOptions[question.id] === options[optionIndex]}
-                      onChange={() => handleOptionSelect(question.id, optionIndex)}
+                      checked={
+                        selectedOptions[question.id] === options[optionIndex]
+                      }
+                      onChange={() =>
+                        handleOptionSelect(question.id, optionIndex)
+                      }
                       className="mr-2"
                     />
-                    <label>{option === 'A' ? 'Certainly' : option === 'B' ? 'Very Likely' : option === 'C' ? 'Likely' : option === 'D' ? 'Unlikely' : 'Certainly Not'}</label>
+                    <label>
+                      {option === "A"
+                        ? question.option1
+                        : option === "B"
+                        ? question.option2
+                        : option === "C"
+                        ? question.option3
+                        : option === "D"
+                        ? question.option4
+                        : question.option5}
+                    </label>
                   </div>
                 ))}
               </div>
             </div>
           ))}
         </div>
+
         <button
           type="submit"
-          className="text-white mb-10 bg-blue-texts hover:bg-[#4999c4] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm w-full py-2.5 text-center"
+          disabled={!allQuestionsAnswered} // Disable the button if all questions are not answered
+          className={`text-white mb-10 bg-blue-texts hover:bg-[#4999c4] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm w-full py-2.5 text-center ${
+            !allQuestionsAnswered ? "opacity-50 cursor-not-allowed" : ""
+          }`}
         >
           Submit
         </button>
